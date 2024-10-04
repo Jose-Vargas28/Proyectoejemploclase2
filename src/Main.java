05200ab87fe15f315959e679982979e7b522ae6a
Jose-Vargas28 <josemvargas.28@gmail.com>
2024-10-04 11:42:52 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nombre;
+        double edad;
+        System.out.println("Ingrese su nombre y su edad: "); //sout+tab
+        nombre = scanner.nextLine();
+        edad = scanner.nextDouble();
+
+        System.out.println(nombre + " " + edad);
+
+        }
+    }
