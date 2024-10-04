32b43caa4a5b086bb6de8b33b85aa8cef9c14d03
Jose-Vargas28 <josemvargas.28@gmail.com>
2024-10-04 11:58:19 +0200

    Cambio prueba #2

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,6 @@ public class Main {
         System.out.println(nombre + " " + edad);
 
         System.out.println("Hola mundo");
+        System.out.println("Prueba de cambio");
         }
     }
